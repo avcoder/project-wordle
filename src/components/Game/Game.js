f32c55a885c13fb2449ff51980929c79fc8d3c18
avcoder <avcoder@users.noreply.github.com>
2024-06-12 15:57:16 -0400

    fix class to className

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
 
-import Guess from '../Guess/Guess.js'
-import GuessBoard from '../GuessBoard/GuessBoard.js'
-import Restart from '../Restart/Restart.js'
+import Guess from "../Guess/Guess.js";
+import GuessBoard from "../GuessBoard/GuessBoard.js";
+import Restart from "../Restart/Restart.js";
 
-import { NUM_OF_GUESSES_ALLOWED, WON, PLAYING, LOST } from '../../constants.js'
+import { NUM_OF_GUESSES_ALLOWED, WON, PLAYING, LOST } from "../../constants.js";
 
 // To make debugging easier, we'll log the solution in the console.
 
@@ -16,58 +16,56 @@ function Game() {
   const [answer, setAnswer] = useState(() => {
     const answer = sample(WORDS);
     console.info(answer);
-    return answer
-  })
-  const [gameStatus, setGameStatus] = useState(PLAYING)
-
+    return answer;
+  });
+  const [gameStatus, setGameStatus] = useState(PLAYING);
 
   function handleGuess(guess) {
-    const newGuesses = [...guesses, guess.guess]
+    const newGuesses = [...guesses, guess.guess];
     if (newGuesses.includes(answer)) {
-      setGameStatus(WON)
+      setGameStatus(WON);
     } else if (newGuesses.length > NUM_OF_GUESSES_ALLOWED - 1) {
       setGameStatus(LOST);
     }
-    setGuesses(newGuesses)
+    setGuesses(newGuesses);
   }
   function restart() {
     setAnswer(() => {
       const answer = sample(WORDS);
       console.info(answer);
-      return answer
+      return answer;
     });
     setGuesses([]);
-    setGameStatus(PLAYING)
+    setGameStatus(PLAYING);
   }
 
   return (
     <>
       <GuessBoard guesses={guesses} answer={answer} />
-      {
-        gameStatus === PLAYING && (
-          <Guess onGuess={handleGuess} />
-        )
-      }
-      {
-        gameStatus === WON && (
-          <div class="happy banner">
-            <p>
-              <strong>Congratulations!</strong> Got it in
-              <strong> {guesses.length} {guesses.length === 1 ? 'guess' : 'guesses'}</strong>.
-            </p>
-            <Restart restart={restart} />
-          </div>
-        )
-      }
-      {
-        gameStatus === LOST && (
-          <div class="sad banner">
-            <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
-            <Restart restart={restart} />
-          </div>
-        )
-      }
-    </>);
+      {gameStatus === PLAYING && <Guess onGuess={handleGuess} />}
+      {gameStatus === WON && (
+        <div className="happy banner">
+          <p>
+            <strong>Congratulations!</strong> Got it in
+            <strong>
+              {" "}
+              {guesses.length} {guesses.length === 1 ? "guess" : "guesses"}
+            </strong>
+            .
+          </p>
+          <Restart restart={restart} />
+        </div>
+      )}
+      {gameStatus === LOST && (
+        <div className="sad banner">
+          <p>
+            Sorry, the correct answer is <strong>{answer}</strong>.
+          </p>
+          <Restart restart={restart} />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Game;
